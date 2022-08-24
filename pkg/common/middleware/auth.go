ec5fc54912d912f2ad7d2c2fd2d011277dd583b4
ooatamelbug <mohamedwork636@gmail.com>
2022-08-25 00:08:37 +0200

    add auth middleware

---
+++ pkg/common/middleware/auth.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	services "github.com/ooatamelbug/blog-task-app/pkg/common/service"
+)
+
+func Auth(jwtService services.JwtService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+
+		if authHeader == "" {
+			response := services.ReturnResponse(false, "not Authorization", nil, "", "no token")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		token, err := jwtService.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Panicln("claims[user_id]:", claims["user_id"])
+			log.Panicln("claims[issuer]:", claims["issuer"])
+		} else {
+			log.Panicln(err)
+			response := services.ReturnResponse(false, "not Authorization", nil, "", err.Error())
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+
+	}
+}
