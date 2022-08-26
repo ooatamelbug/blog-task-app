06bccf100a093c965b75b8cc97a2943298850efe
ooatamelbug <mohamedwork636@gmail.com>
2022-08-26 02:28:17 +0200

    app in heroku heroku

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,6 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/ooatamelbug/blog-task-app/pkg/auth"
 	"github.com/ooatamelbug/blog-task-app/pkg/comments"
@@ -74,26 +66,5 @@ func main() {
 		commentRoutes.DELETE("/delete/:id", commentController.DeleteComment).Use(middleware.Auth(jwtService))
 	}
 
-	srv := &http.Server{
-		Addr:    ":5000",
-		Handler: server,
-	}
-
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("faild to initilze server: %v\n", err)
-		}
-	}()
-	log.Printf("listen on port %v\n", srv.Addr)
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	log.Println("shutting down server")
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("server force Shutdown %v\n", err)
-	}
+	server.Run(":5000")
 }
